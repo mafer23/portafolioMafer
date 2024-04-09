81a369df64f00826258ecef03a44829f78e71144
Maria Fernanda Palencia <39041288+mafer23@users.noreply.github.com>
2024-04-08 23:28:54 -0500

    Ultimos cambios

--- src/app/components/component.routing.ts
+++ src/app/components/component.routing.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SectionComponent } from './section/section.component';
 
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'navbar',
+    component: NavbarComponent
+  },
+  {
+    path: 'section',
+    component: SectionComponent
   },
   {
     path: '',
